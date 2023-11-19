df74984df45569d5b2604d389910f05dfaf8ea62
Denise <deniselewis1120@gmail.com>
2023-11-18 22:31:30 -0800

    Create datepicker for start and end dates

--- app/(tabs)/form.tsx
+++ app/(tabs)/form.tsx
@@ -1,9 +1,64 @@
-import { StyleSheet } from 'react-native';
+import DateTimePicker, {
+  DateTimePickerAndroid,
+} from '@react-native-community/datetimepicker';
+import { useState } from 'react';
+import { StyleSheet, Button } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabTwoScreen() {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [startMode, setStartMode] = useState('date');
+  const [endMode, setEndMode] = useState('date');
+  const [startShow, setStartShow] = useState(false);
+  const [endShow, setEndShow] = useState(false);
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  };
+
+  const onChangeStart = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setStartShow(false);
+    setStartDate(currentDate);
+  };
+
+  const showModeStart = (currentMode) => {
+    setStartShow(true);
+    setStartMode(currentMode);
+  };
+
+  const onChangeEnd = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setEndShow(false);
+    setEndDate(currentDate);
+  };
+
+  const showModeEnd = (currentMode) => {
+    setEndShow(true);
+    setEndMode(currentMode);
+  };
+
+  const showDatepickerStart = () => {
+    showModeStart('date');
+  };
+
+  const showDatepickerEnd = () => {
+    showModeEnd('date');
+  };
+
+  /*
+  const showTimepicker = () => {
+    showMode('time');
+  };
+*/
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Enter Your Trip Details</Text>
@@ -12,6 +67,33 @@ export default function TabTwoScreen() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <View>
+        <Button onPress={showDatepickerStart} title="Select Start Date" />
+        <Text>{startDate.toLocaleString('en-US', options)}</Text>
+        {startShow && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={startDate}
+            mode={startMode}
+            is24Hour
+            onChange={onChangeStart}
+          />
+        )}
+      </View>
+      <></>
+      <View>
+        <Button onPress={showDatepickerEnd} title="Select End Date" />
+        <Text>{endDate.toLocaleString('en-US', options)}</Text>
+        {endShow && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={endDate}
+            mode={endMode}
+            is24Hour
+            onChange={onChangeEnd}
+          />
+        )}
+      </View>
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
