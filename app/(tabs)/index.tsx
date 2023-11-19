71a8bf806a6ea77ca7e485afa35dc482ca4b4fce
elaine2700 <easerrano.e@gmail.com>
2023-11-19 14:52:23 -0800

    Installing Fonts and Routing Home Page to Trip Form

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,42 @@
+import * as Font from 'expo-font';
+import { Link } from 'expo-router';
+import { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
-import { ActivityIndicator } from 'react-native-paper';
+import { ActivityIndicator, Button } from 'react-native-paper';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+        'JosefinSans-Regular': require('../../assets/fonts/JosefinSans-Regular.ttf'),
+        'JosefinSans-SemiBold': require('../../assets/fonts/JosefinSans-SemiBold.ttf'),
+        'OpenSans-Regular': require('../../assets/fonts/OpenSans-Regular.ttf'),
+      });
+
+      setFontLoaded(true);
+    }
+
+    loadFont();
+  }, []);
+
+  if (!fontLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Welcome to your favorite Packing List Helper!
-      </Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <ActivityIndicator />
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.title}> to Packing Helper</Text>
+
+      <Link href="/(tabs)/form">
+        <Button textColor="#FFFFDD" style={styles.button}>
+          Start Packing
+        </Button>
+      </Link>
     </View>
   );
 }
@@ -28,12 +48,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontFamily: 'JosefinSans-Regular',
+    fontSize: 30,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  button: {
+    marginTop: 10,
+    backgroundColor: '#A2C579',
+    color: '#FFFFDD',
+    fontFamily: 'OpenSans-Regular',
   },
 });
